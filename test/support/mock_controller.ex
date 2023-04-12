83aeb708d67c7a4be82a5e044d606cfc7000f594
Dylan Blakemore <dylan.blakemore@gmail.com>
2023-04-12 20:34:03 +0200

    Remove old endpoint

--- test/support/mock_controller.ex
+++ test/support/mock_controller.ex
@@ -31,42 +31,6 @@ defmodule HttpWrapper.MockController do
     end
   end
 
-  post "/v1/chat/completions" do
-    case conn.params do
-      %{
-        "max_tokens" => 256,
-        "model" => "gpt-3.5-turbo",
-        "prompt" => [%{"text" => "Hello", "type" => "human"}],
-        "temperature" => 0.7
-      } ->
-        success(conn, %{
-          "choices" => [
-            %{
-              "finish_reason" => "stop",
-              "index" => 0,
-              "message" => %{
-                "content" =>
-                  "The 2020 World Series was played at Globe Life Field in Arlington, Texas due to the COVID-19 pandemic.",
-                "role" => "assistant"
-              }
-            }
-          ],
-          "created" => 1_677_773_799,
-          "id" => "chatcmpl-6pftfA4NO9pOQIdxao6Z4McDlx90l",
-          "model" => "gpt-3.5-turbo-0301",
-          "object" => "chat.completion",
-          "usage" => %{
-            "completion_tokens" => 26,
-            "prompt_tokens" => 56,
-            "total_tokens" => 82
-          }
-        })
-
-      _ ->
-        failure(conn)
-    end
-  end
-
   defp success(conn, body) do
     conn
     |> Plug.Conn.send_resp(:ok, Jason.encode!(body))
